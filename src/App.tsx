3d0636e21b3a61a8beb51742b8f29113c5d76cec
Voskan Voskanyan <voskanvoskanyan@yandex.com>
2022-07-28 14:14:50 +0400

    outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import Footer from './components/Footer';
-import Header from './components/Header';
 import { Home, Contact, Article, Articles, About, NotFound } from './pages';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <main>
-        <Routes>
+      <Routes>
+        <Route path='/' element={<Layout />}>
           <Route path='/' element={<Home />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/about' element={<About />} />
           <Route path='/articles' element={<Articles />} />
           <Route path='*' element={<NotFound />} />
-        </Routes>
-      </main>
-      <Footer />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
